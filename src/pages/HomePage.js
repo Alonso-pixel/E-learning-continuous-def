d45567a402ad6677e857a0b728318e7be685e576
Alonso-pixel <kreitos_05@outlook.com>
2024-03-03 19:34:45 -0600

    removed some unused imports

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Hero from "../components/Hero";
 import CoursesList from "../components/CourseList";
-import CategoriesList from "../components/CategoriesList";
 
 const HomePage = () => {
   return (
